33a7dd6644c40cafb8d7171f55e10fb988da5bfd
Russell Dunk <russelldunk@gmail.com>
2016-10-14 13:41:20 -0400

    Added a hardcoded setup script

--- setup.py
+++ setup.py
@@ -1,1 +1,15 @@
-print("This is the setup.py file there is nothing here yet\nGoodbye")
+#!/usr/bin/env python
+#Hardcoded dependancy installer
+
+import os 
+
+
+libpq = "apt-get install libpq-dev"
+python3 = "apt-get install python3"
+python3dev = "apt-get install python3-dev"
+psycopg2 = "apt-get install python-psycopg2"
+
+os.system(libpq)
+os.system(python3)
+os.system(python3dev)
+os.system(psycopg2)
