7a2f517efc5029b1620ce8ee03c4d71094fc7f85
andrew downie <andrew_dev@outlook.com>
2016-10-20 15:05:15 +0000

    You can now send a mock request and get mock data for weight, and or height

--- ClinicalAnalysisEngine/main/process_request.py
+++ ClinicalAnalysisEngine/main/process_request.py
@@ -3,28 +3,54 @@
 
 import json
 
+#####
+##### Process Request
+#####
 def ProcessRequest(jsonRequest):
     loadedJson = ""
 
-    #Load the Json request
+
+    ###
+    ### Load the Json request
+    ###
     try:
         loadedJson = json.loads(jsonRequest)
     except:
         return("Not valid json request, please check formatting")
 
-    #Check that required keys are present
+
+    ###
+    ### Check that required keys are present
+    ###
     if 'operation' not in loadedJson:
         return "Not valid json request, missing key: operation"
     elif 'animals' not in loadedJson:
         return "Not valid json request, missing key: animals"
-    elif 'fields' not in loadedJson:
+    elif 'field' not in loadedJson:
         return "Not valid json request, missing key: fields"
 
 
+    ###
+    ### Collect values of required keys 
+    ###
     operation = loadedJson['operation']
     animals = loadedJson['animals']
-    fields = loadedJson['fields']  
+    fields = loadedJson['field']  
 
-    return str(fields)
+
+    ###
+    ### Return mock data related to request fields
+    ###
+    jsonOutput = {}
+    
+    if 'weight' in fields:
+        jsonOutput['weight'] = [10, 17, 22]
+
+    if 'height' in fields:
+        jsonOutput['height'] = [12, 20, 4]
+
+
+    #return str(fields)
+    return json.dumps(jsonOutput)
 
     
