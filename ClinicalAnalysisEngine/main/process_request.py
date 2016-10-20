30cc3065199a4cb890466e6b5d96233f93a4a6f7
andrew downie <andrew_dev@outlook.com>
2016-10-20 14:24:20 +0000

    You can now send mock json requests as per current clinical communication spec.

--- ClinicalAnalysisEngine/main/process_request.py
+++ ClinicalAnalysisEngine/main/process_request.py
@@ -4,10 +4,27 @@
 import json
 
 def ProcessRequest(jsonRequest):
-	loadedJson = ""
+    loadedJson = ""
 
-	try:
-	    loadedJson = json.loads(jsonRequest)
-	    return loadedJson['operation']
-	except:
-            return("Not valid json request, missing operation key")
+    #Load the Json request
+    try:
+        loadedJson = json.loads(jsonRequest)
+    except:
+        return("Not valid json request, please check formatting")
+
+    #Check that required keys are present
+    if 'operation' not in loadedJson:
+        return "Not valid json request, missing key: operation"
+    elif 'animals' not in loadedJson:
+        return "Not valid json request, missing key: animals"
+    elif 'fields' not in loadedJson:
+        return "Not valid json request, missing key: fields"
+
+
+    operation = loadedJson['operation']
+    animals = loadedJson['animals']
+    fields = loadedJson['fields']  
+
+    return str(fields)
+
+    
