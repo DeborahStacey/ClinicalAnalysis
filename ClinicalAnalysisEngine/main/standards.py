d6bbf6d66eba6ae939083334016f8adbe4fa4cfd
aeckensw <aeckensw@mail.uoguelph.ca>
2016-11-08 08:53:15 -0500

    removing standards class

--- ClinicalAnalysisEngine/main/standards.py
+++ ClinicalAnalysisEngine/main/standards.py
@@ -1,44 +1,43 @@
-class Standards():
 
-    def InvalidJSON():
-        return Return(INVALID_JSON_CODE, INVALID_JSON_MESSAGE)
+def InvalidJSON():
+    return Return(INVALID_JSON_CODE, INVALID_JSON_MESSAGE)
 
-    def InvalidOperation():
-        return Return(INVALID_OPERATION_CODE, INVALID_OPERATION_MESSAGE)
+def InvalidOperation():
+    return Return(INVALID_OPERATION_CODE, INVALID_OPERATION_MESSAGE)
 
-    def MissingOperation():
-        return Return(MISSING_OPERATION_CODE, MISSING_OPERATION_MESSAGE)
+def MissingOperation():
+    return Return(MISSING_OPERATION_CODE, MISSING_OPERATION_MESSAGE)
 
-    def MissingAnimal():
-        return Return(MISSING_ANIMAL_CODE, MISSING_ANIMAL_MESSAGE)
+def MissingAnimal():
+    return Return(MISSING_ANIMAL_CODE, MISSING_ANIMAL_MESSAGE)
 
-    def MissingField():
-        return Return(MISSING_FIELD_CODE, MISSING_FIELD_MESSAGE)
+def MissingField():
+    return Return(MISSING_FIELD_CODE, MISSING_FIELD_MESSAGE)
 
-    def CaughtException():
-        return Return(EXCEPTION_CODE, EXCEPTION_MESSAGE)
+def CaughtException():
+    return Return(EXCEPTION_CODE, EXCEPTION_MESSAGE)
 
-    INVALID_JSON_CODE = 'ERROR_01'
-    INVALID_JSON_MESSAGE = "Invalid json request, improperly formatted json"
+INVALID_JSON_CODE = 'ERROR_01'
+INVALID_JSON_MESSAGE = "Invalid json request, improperly formatted json"
 
-    INVALID_OPERATION_CODE = ''
-    INVALID_OPERATION_MESSAGE = 'Invalid operation selected'
+INVALID_OPERATION_CODE = ''
+INVALID_OPERATION_MESSAGE = 'Invalid operation selected'
 
-    MISSING_OPERATION_CODE = 'ERROR_10'
-    MISSING_OPERATION_MESSAGE = "Invalid json request, missing key: operation"
+MISSING_OPERATION_CODE = 'ERROR_10'
+MISSING_OPERATION_MESSAGE = "Invalid json request, missing key: operation"
 
-    MISSING_ANIMAL_CODE = 'ERROR_11'
-    MISSING_ANIMAL_MESSAGE = "Invalid json request, missing key: animal"
+MISSING_ANIMAL_CODE = 'ERROR_11'
+MISSING_ANIMAL_MESSAGE = "Invalid json request, missing key: animal"
 
-    MISSING_FIELD_CODE = 'ERROR_12'
-    MISSING_FIELD_MESSAGE = "Invalid json request, missing key: field"
+MISSING_FIELD_CODE = 'ERROR_12'
+MISSING_FIELD_MESSAGE = "Invalid json request, missing key: field"
 
-    EXCEPTION_CODE = 'ERROR_99'
-    EXCEPTION_MESSAGE = "Exception caught"
+EXCEPTION_CODE = 'ERROR_99'
+EXCEPTION_MESSAGE = "Exception caught"
 
-    def Return(code, message):
-        jsonMsg = {}
-        jsonMsg["result"] = "error"
-        jsonMsg["code"] = code
-        jsonMsg["message"] = message
-        return json.dumps(jsonMsg)
+def Return(code, message):
+    jsonMsg = {}
+    jsonMsg["result"] = "error"
+    jsonMsg["code"] = code
+    jsonMsg["message"] = message
+    return json.dumps(jsonMsg)
